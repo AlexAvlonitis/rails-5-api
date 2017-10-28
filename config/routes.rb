bbc6627d7688065457dafd4213d656e9d0916a23
Alex Avlonitis <avlonitis@msn.com>
2017-10-28 11:25:42 +0100

    added api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+
+  scope module: :v1,
+        constraints: ApiConstraints.new(version: 1, default: true) do
+  end
 end
