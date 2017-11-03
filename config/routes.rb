93c88da368939ff0581596b714c36a9e885a814b
Alex Avlonitis <avlonitis@msn.com>
2017-11-03 20:38:09 +0000

    added namespacing and users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  scope module: :v1,
-        constraints: ApiConstraints.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json } do
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      resources :users
+    end
   end
 end
